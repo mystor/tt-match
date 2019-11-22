b3add1393c631e1621e4e5edf06389c4e3d0655e
Nika Layzell <nika@thelayzells.com>
2019-11-22 18:21:41 -0500

    Initial Design Sketch

---
+++ src/lib.rs
@@ -0,0 +1,274 @@
+#[doc(hidden)]
+pub use syn::{
+    braced as _syn_braced, bracketed as _syn_bracketed, parenthesized as _syn_parenthesized,
+    Token as _syn_Token,
+};
+
+// Not public API.
+#[doc(hidden)]
+#[path = "runtime.rs"]
+pub mod __rt;
+
+/// The whole point
+#[macro_export]
+macro_rules! tt_matcher {
+    ($vis:vis match fn $name:ident($($t:tt)*) -> $T:ty { $($e:tt)* }) => {
+        $vis fn $name(input: $crate::__rt::ParseStream) -> $crate::__rt::Result<$T> {
+            // let var = Pending::new();
+            $crate::match_each_cap!([match_wrapper_cb! new] $($t)*);
+            // -- parsing --
+            $crate::match_each_token!([match_token_cb! input] $($t)*);
+            // let var = var.finish();
+            $crate::match_each_cap!([match_wrapper_cb! finish] $($t)*);
+
+            // Pattern Code
+            Ok((|| -> $T { $($e:tt)* })())
+        }
+    };
+}
+
+// Tries to run a subparser. Expands to an expression evaluating to `true` on
+// success, and `false` on failure.
+//
+// FIXME: This produces really bad error messages. It should not try to parse
+// the entirety of `$inner`, but rather only the first token, to decide whether
+// or not to recurse.
+//
+// FIXME: Figure out a good way to implement a better parser here (ideally
+// without `Speculative`).
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_try_parse {
+    ($input:ident $($inner:tt)*) => {
+        (|| -> $crate::__rt::Result<()> {
+            let fork = $input.fork();
+            // let var = var.enter();
+            $crate::match_each_cap!([match_wrapper_cb! enter] $($inner)*);
+            // -- parsing --
+            $crate::match_each_token!([match_token_cb! fork] $($inner)*);
+            // var.exit();
+            $crate::match_each_cap!([match_wrapper_cb! exit] $($inner)*);
+
+            {
+                use $crate::__rt::Speculative;
+                $input.advance_to(&fork);
+            }
+            $crate::__rt::Ok(())
+        })().is_ok()
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_token_cb {
+    // NOTE: These use `drop(content)` explicitly, rather than introducing a
+    // scope, as logic depends on a flat namespace while matching non-{rep,opt}
+    // tokens.
+    ($input:ident Tok ( $($inner:tt)* )) => {
+        let content;
+        $crate::_syn_parenthesized!(content in $input);
+        $crate::match_each_token!([match_token_cb! content] $($inner)*);
+        $crate::__rt::drop(content);  // Scope `content` var.
+    };
+    ($input:ident Tok [ $($inner:tt)* ]) => {
+        let content;
+        $crate::_syn_bracketed!(content in $input);
+        $crate::match_each_token!([match_token_cb! content] $($inner)*);
+        $crate::__rt::drop(content);  // Scope `content` var.
+    };
+    ($input:ident Tok { $($inner:tt)* }) => {
+        let content;
+        $crate::_syn_braced!(content in $input);
+        $crate::match_each_token!([match_token_cb! content] $($inner)*);
+        $crate::__rt::drop(content);  // Scope `content` var.
+    };
+
+    // FIXME: Consider matching certain `$other` values, such as idents &
+    // operators, in a more efficient manner.
+    ($input:ident Tok $other:tt) => {
+        $crate::__rt::match_parse(&mut $input, stringify!($other))?;
+    };
+
+    // #(...)*
+    ($input:ident Rep $($inner:tt)*) => {
+        $crate::match_each_cap!([match_wrapper_cb! set_default] $($inner)*);
+        while $crate::match_try_parse!($input $($inner)*) { }
+    };
+    // #(...),*
+    ($input:ident RepSep $sep:tt $($inner:tt)*) => {
+        $crate::match_each_cap!([match_wrapper_cb! set_default] $($inner)*);
+        if $crate::match_try_parse!($input $($inner)*) {
+            // Repeatedly try to parse with a leading `sep`.
+            while $crate::match_try_parse!($input $sep $($inner)*) { }
+        }
+    };
+
+    // Default `Parse` handling for a single capture.
+    ($input:ident Cap $var:ident : $kind:ident) => {
+        let $var = $var.set($input.parse()?);
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_wrapper_cb {
+    (new $var:ident : $T:ty) => {
+        let $var = <$crate::__rt::Pending<$T>>::new();
+    };
+    (finish $var:ident : $T:ty) => {
+        let $var = $var.finish();
+    };
+    (set_default $var:ident : $T:ty) => {
+        let mut $var = $var.set_default();
+    };
+    (enter $var:ident : $T:ty) => {
+        let $var = $var.enter();
+    };
+    (exit $var:ident : $T:ty) => {
+        $var.exit();
+    };
+}
+
+// match_each_cap!(cb!(...) #foo:ident #(opt #(#bar:ident)*)?) =>
+//   cb!(... foo: MatchCapTy![[] ident]);
+//   cb!(... foo: MatchCapTy![[Option Vec] ident]);
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_each_cap {
+    ($cb:tt $($t:tt)*) => {
+        $crate::match_each_token!(
+            [match_each_cap_cb! $cb []] $($t)*
+        );
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_each_cap_cb {
+    // non-repeating `Group`s
+    ($cb:tt $wr:tt Tok ( $($inner:tt)* )) => {
+        $crate::match_each_token!([match_each_cap_cb! $cb $wr] $($inner)*);
+    };
+    ($cb:tt $wr:tt Tok [ $($inner:tt)* ]) => {
+        $crate::match_each_token!([match_each_cap_cb! $cb $wr] $($inner)*);
+    };
+    ($cb:tt $wr:tt Tok { $($inner:tt)* }) => {
+        $crate::match_each_token!([match_each_cap_cb! $cb $wr] $($inner)*);
+    };
+    ($cb:tt $wr:tt Tok $t:tt) => {};
+
+    // complex non-terminals
+    ($cb:tt [$($wr:ident)*] Rep $($inner:tt)*) => {
+        $crate::match_each_token!(
+            [match_each_cap_cb! $cb [$($wr)* Vec]] $($inner)*
+        );
+    };
+    ($cb:tt [$($wr:ident)*] RepSep $sep:tt $($inner:tt)*) => {
+        $crate::match_each_token!(
+            [match_each_cap_cb! $cb [$($wr)* Vec]] $($inner)*
+        );
+    };
+
+    // captures
+    ($cb:tt $wr:tt Cap $var:ident : $kind:ident) => {
+        $crate::match_call_cb!($cb $var: $crate::MatchCapTy![$wr $kind]);
+    };
+}
+
+// MatchCapTy![[Vec Option] tt] => Vec<Option<TokenTree>>
+#[macro_export]
+#[doc(hidden)]
+macro_rules! MatchCapTy {
+    ([] ident) => { $crate::__rt::Ident };
+    ([] tt) => { $crate::__rt::TokenTree };
+
+    ([$T:ident $($wr:ident)*] $kind:ident) => {
+        $crate::__rt::$T::<$crate::MatchCapTy![[$($wr)*] $kind]>
+    };
+}
+
+// Call a callback for each "token" in the stream, after classifying it. Does
+// not recurse into groups.
+//
+// match_each_token!([cb! ...] a #b:ident [group]) =>
+//   cb!(... Tok a);
+//   cb!(... Cap b : ident);
+//   cb!(... Tok [group]);
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_each_token {
+    ($cfg:tt $($tts:tt)*) => {
+        $crate::match_tokens_with_context!(
+            $cfg
+            (@ @ @ @ @ @ $($tts)*)
+            (@ @ @ @ @ $($tts)* @)
+            (@ @ @ @ $($tts)* @ @)
+            (@ @ @ $(($tts))* @ @ @)
+            (@ @ $($tts)* @ @ @ @)
+            (@ $($tts)* @ @ @ @ @)
+            ($($tts)* @ @ @ @ @ @)
+        );
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_each_token_with_context {
+    (
+        $cfg:tt
+        ($($b3:tt)*) ($($b2:tt)*) ($($b1:tt)*)
+        ($($curr:tt)*)
+        ($($a1:tt)*) ($($a2:tt)*) ($($a3:tt)*)
+    ) => {
+        $(
+            $crate::match_token_with_context!($cfg $b3 $b2 $b1 $curr $a1 $a2 $a3);
+        )*
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_token_with_context {
+    // leading & trailing '@'s
+    ($cb:tt $b3:tt $b2:tt $b1:tt @ $a1:tt $a2:tt $a3:tt) => {};
+
+    // #(...)* => cb!(Rep ...)
+    ($cb:tt $b3:tt $b2:tt $b1:tt (#) ( $($inner:tt)* ) * $a3:tt) => {
+        $crate::match_call_cb!($cb Rep $($inner)*);
+    };
+    ($cb:tt $b3:tt $b2:tt # (( $($inner:tt)* )) * $a2:tt $a3:tt) => {};
+    ($cb:tt $b3:tt # ( $($inner:tt)* ) (*) $a1:tt $a2:tt $a3:tt) => {};
+
+    // #(...),* => cb!(RepSep , ...)
+    ($cb:tt $b3:tt $b2:tt $b1:tt (#) ( $($inner:tt)* ) $sep:tt *) => {
+        $crate::match_call_cb!($cb RepSep $sep $($inner)*);
+    };
+    ($cb:tt $b3:tt $b2:tt # (( $($inner:tt)* )) $sep:tt * $a3:tt) => {};
+    ($cb:tt $b3:tt # ( $($inner:tt)* ) ($sep:tt) * $a2:tt $a3:tt) => {};
+    ($cb:tt # ( $($inner:tt)* ) $sep:tt (*) $a1:tt $a2:tt $a3:tt) => {};
+
+    // #var:kind => cb!(Cap var : kind)
+    ($cb:tt $b3:tt $b2:tt $b1:tt (#) $var:ident : $kind:ident) => {
+        $crate::match_call_cb!($cb Cap $var : $kind);
+    };
+    ($cb:tt $b3:tt $b2:tt # ($var:ident) : $kind:ident $a3:tt) => {};
+    ($cb:tt $b3:tt # $var:ident (:) $kind:ident $a2:tt $a3:tt) => {};
+    ($cb:tt # $var:ident : ($kind:ident) $a1:tt $a2:tt $a3:tt) => {};
+
+    // ?? => cb!(Tok ??)
+    ($cb:tt $b3:tt $b2:tt $b1:tt ($curr:tt) $a1:tt $a2:tt $a3:tt) => {
+        $crate::match_call_cb!($cb Tok $curr);
+    };
+}
+
+// Call single-tt callback descriptor
+//
+// match_call_cb!([cb! ..extra] ..args) =>
+//   cb!(..extra ..args)
+#[macro_export]
+#[doc(hidden)]
+macro_rules! match_call_cb {
+    ([$call:ident! $($extra:tt)*] $($arg:tt)*) => {
+        $crate::$call!($($extra)* $($arg)*)
+    }
+}
---
+++ src/runtime.rs
@@ -0,0 +1,150 @@
+//! Not public API. Runtime structs, re-exports, traits, and functions used by
+//! macros in this crate.
+
+use proc_macro2::Spacing;
+use std::marker::PhantomData;
+
+// re-exports
+pub use std::mem::drop;
+pub use std::option::Option::{self, None, Some};
+pub use std::result::Result::{Err, Ok};
+pub use std::vec::Vec;
+
+pub use syn::parse::discouraged::Speculative;
+pub use syn::parse::ParseStream;
+pub use syn::{Expr, Ident, Result};
+
+pub use proc_macro2::{TokenStream, TokenTree};
+
+// Container which can be the parent of `Pending` or `Active`.
+pub trait ParentContainer<T> {
+    fn insert(&mut self, value: T);
+}
+
+// Pending represents a value which hasn't been fully initialized yet.
+pub struct Pending<T, P = ()> {
+    parent: P,
+    marker: PhantomData<T>,
+}
+
+impl<T> Pending<T, ()> {
+    pub fn new() -> Self {
+        Pending {
+            parent: (),
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<T, P> Pending<T, P> {
+    pub fn set(self, value: T) -> Active<T, P> {
+        Active {
+            parent: self.parent,
+            value,
+        }
+    }
+}
+
+impl<T, P> Pending<T, P>
+where
+    T: Default,
+{
+    pub fn set_default(self) -> Active<T, P> {
+        Active {
+            parent: self.parent,
+            value: Default::default(),
+        }
+    }
+}
+
+// Active represents a value which has already been fully initialized.
+pub struct Active<T, P = ()> {
+    parent: P,
+    value: T,
+}
+
+impl<T> Active<T, ()> {
+    pub fn finish(self) -> T {
+        self.value
+    }
+}
+
+impl<'a, T, P> Active<T, &'a mut P>
+where
+    P: ParentContainer<T>,
+{
+    pub fn exit(self) {
+        self.parent.insert(self.value);
+    }
+}
+
+// Container helper methods for entering, inserting, and exiting values.
+impl<T, P> Active<Vec<T>, P> {
+    pub fn enter(&mut self) -> Pending<T, &mut Self> {
+        Pending {
+            parent: self,
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<T, P> ParentContainer<T> for Active<Vec<T>, P> {
+    fn insert(&mut self, value: T) {
+        self.value.push(value);
+    }
+}
+
+impl<T, P> Active<Option<T>, P> {
+    pub fn enter(&mut self) -> Pending<T, &mut Self> {
+        Pending {
+            parent: self,
+            marker: PhantomData,
+        }
+    }
+}
+
+impl<T, P> ParentContainer<T> for Active<Option<T>, P> {
+    fn insert(&mut self, value: T) {
+        assert!(self.value.is_none());
+        self.value = Some(value);
+    }
+}
+
+// Parse an input from expected.
+pub fn match_parse(input: ParseStream, expected: &str) -> Result<()> {
+    let stream = expected.parse::<TokenStream>().unwrap();
+    for expect in stream {
+        input.step(|cursor| {
+            let (tt, next) = cursor
+                .token_tree()
+                .ok_or_else(|| cursor.error(format_args!("Expected `{}`", expect)))?;
+
+            let equal = match (&tt, &expect) {
+                // Allow the case where `b.spacing() == Spacing::Alone` yet
+                // `a.spacing() == Spacing::Joint`. In that scenario we're
+                // matching the prefix of an operator, which should be allowed.
+                (TokenTree::Punct(a), TokenTree::Punct(b)) => {
+                    a.as_char() == b.as_char()
+                        && (b.spacing() == Spacing::Alone || a.spacing() == b.spacing())
+                }
+
+                (TokenTree::Ident(a), TokenTree::Ident(b)) => a == b,
+                (TokenTree::Literal(a), TokenTree::Literal(b)) => a.to_string() == b.to_string(),
+
+                // A group may be able to show up if an `$expr` or similar was
+                // injected into the pattern.
+                //
+                // FIXME: Should we call `match_parse` recursively, or is it OK
+                // to just `to_string()`.
+                (TokenTree::Group(a), TokenTree::Group(b)) => a.to_string() == b.to_string(),
+                _ => false,
+            };
+            if !equal {
+                return Err(cursor.error(format_args!("Expected `{}`", expect)))?;
+            }
+            Ok(((), next))
+        })?;
+    }
+
+    Ok(())
+}
